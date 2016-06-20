eb7eac3e0edc740d950768d5fd8dba031983f2dd
alicelei <leiyunlei@gmail.com>
2016-06-20 19:42:10 -0400

    Added start,stop,and switch button to Homepage

--- iOS/HomePageViewController.cs
+++ iOS/HomePageViewController.cs
@@ -36,48 +36,6 @@ namespace ProcessDashboard.iOS
 				//BackgroundColor = UIColor.LightGray,
 			};
 
-			var startLabel = new UILabel(new CGRect(200, 160, 30, 20))
-			{
-				Text = "Start",
-				Font = UIFont.SystemFontOfSize(12),
-				TextColor = UIColor.Blue,
-				//TextAlignment = UITextAlignment.Left,
-				//BackgroundColor = UIColor.LightGray,
-			};
-
-
-
-			var stopLabel = new UILabel(new CGRect(230, 160, 30, 20))
-			{
-				Text = "Stop",
-				Font = UIFont.SystemFontOfSize(12),
-				TextColor = UIColor.Blue,
-				//TextAlignment = UITextAlignment.Left,
-				//BackgroundColor = UIColor.LightGray,
-			};
-
-			var checkLabel = new UILabel(new CGRect(260, 160, 30, 20))
-			{
-				Text = "Check",
-				Font = UIFont.SystemFontOfSize(12),
-				TextColor = UIColor.Blue,
-				//TextAlignment = UITextAlignment.Left,
-				//BackgroundColor = UIColor.LightGray,
-			};
-
-			//startButton = UIButton.FromType(UIButtonType.RoundedRect);
-			//startButton.SetTitle("Start", UIControlState.Normal);
-			//startButton.SetImage(UIImage.FromFile("start.png"), UIControlState.Normal);
-
-			//var stopButton = new UIButton(new CGRect(250, 160, 300, 20));
-			//stopButton = UIButton.FromType(UIButtonType.RoundedRect);
-			//stopButton.SetTitle("Stop", UIControlState.Normal);
-			//	//.SetImage(UIImage.FromFile("stop.png"), UIControlState.Normal);
-
-			//var checkButton = new UIButton(new CGRect(300, 160, 300, 20));
-			//checkButton = UIButton.FromType(UIButtonType.RoundedRect);
-			//checkButton.SetTitle("Check", UIControlState.Normal);
-			//	//.SetImage(UIImage.FromFile("check.png"), UIControlState.Normal);
 
 
 			var CurrentTaskNameLabel = new UILabel(new CGRect(30, 190, 300, 60))
@@ -91,6 +49,44 @@ namespace ProcessDashboard.iOS
 				LineBreakMode = UILineBreakMode.WordWrap,
 			};
 
+			UIButton startButton, stopButton;
+			UISwitch switchEnabled;
+
+			startButton = UIButton.FromType(UIButtonType.RoundedRect);
+			startButton.SetImage(UIImage.FromFile("start.png"), UIControlState.Normal);
+			startButton.Frame = new CGRect(100, 255, 45, 42);
+			startButton.TouchUpInside += (sender, e) =>
+			{
+				new UIAlertView("Notice"
+					, "Start logging time!"
+					, null
+					, "OK"
+					, null).Show();
+			};
+			stopButton = UIButton.FromType(UIButtonType.Custom);
+			stopButton.SetImage(UIImage.FromFile("stop.png"), UIControlState.Normal);
+			stopButton.Frame = new CGRect(170, 255, 45, 42);
+			stopButton.TouchUpInside += (sender, e) =>
+			{
+				new UIAlertView("Notice"
+					, "Stop logging time!"
+					, null
+					, "OK"
+					, null).Show();
+			};
+
+
+			switchEnabled = new UISwitch(new CGRect(240, 260, 50, 30));
+			switchEnabled.ValueChanged += (sender, e) =>
+			{
+				new UIAlertView("Notice"
+					, "This task marks as completed!"
+					, null
+					, "OK"
+					, null).Show();
+			};
+			switchEnabled.On = true;
+
 
 			string[] tableItems = new string[] { "/Project/Mobile App I1/High Level Design Document/View Logic/UI experiment/Draft",
 				"/Project/Mobile App I1/High Level Design Document/View Logic/UI experiment/Personal Review",
@@ -99,19 +95,22 @@ namespace ProcessDashboard.iOS
 				"/Project/Mobile App I1/High Level Design Document/View Logic/Task2/Publish", 
 				"/Project/Mobile App I1/High Level Design Document/View Logic/Task3/Refine", 
 				"/Project/Mobile App I1/High Level Design Document/View Logic/Task4/Draft", 
-				"/Project/Mobile App I1/High Level Design Document/View Logic/Task3/Publish" };
+				"/Project/Mobile App I1/High Level Design Document/View Logic/Task3/Publish",
+				"/Project/Mobile App I1/High Level Design Document/View Logic/Task3/Publish",
+				"/Project/Mobile App I1/High Level Design Document/View Logic/Task3/Publish",
+				"/Project/Mobile App I1/High Level Design Document/View Logic/Task3/Publish"};
 
-			RecentTaskTable = new UITableView(new CGRect(0, 250, View.Bounds.Width, View.Bounds.Height));
+			RecentTaskTable = new UITableView(new CGRect(25, 300, View.Bounds.Width - 50, View.Bounds.Height - 300 ));
 			RecentTaskTable.Source = new TaskTableSource(tableItems,this);
 
 			Add(RecentTaskTable);
 
-
+		
+			View.AddSubview(startButton);
+			View.AddSubview(stopButton);
+			View.AddSubview(switchEnabled);
 			this.Add(ProjectNameLabel);
 			this.Add(CurrentTaskLabel);
-			this.Add(startLabel);
-			this.Add(stopLabel);
-			this.Add(checkLabel);
 			this.Add(CurrentTaskNameLabel);
 
 
@@ -130,5 +129,6 @@ namespace ProcessDashboard.iOS
 			}
 
 		}
+
     }
 }
